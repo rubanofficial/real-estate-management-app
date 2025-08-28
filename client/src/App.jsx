cc819fae4b97956ebc621397f1142579c58f4303
rubanofficial <rubans082005@gmail.com>
2025-08-28 20:08:46 +0530

    updated vercel.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/Navbar';
 import Header from './component/Header';
 import Seller from './component/Seller';
