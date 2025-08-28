2c4f5c3f3779852df514f9501b7afc1740044840
rubanofficial <rubans082005@gmail.com>
2025-08-28 20:20:16 +0530

    updated index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
-  // make sure this is "/"
+  base: process.env.NODE_ENV === "production" ? "./" : "/",
 });
