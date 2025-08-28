0be7dbe91604d90ba2cd9529d4aa38efa3984f51
rubanofficial <rubans082005@gmail.com>
2025-08-28 20:16:18 +0530

    updated index.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // make sure this is "/"
+  base: "./",
+  // make sure this is "/"
 });
